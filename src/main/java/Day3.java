113b703e24a9f80190eeb734d3cefb9c9d3544d1
Wojciech Matuszewski <wmatuszewski@n7mobile.com>
2017-12-04 08:46:08 +0100

    day 3 part 1

--- src/main/java/Day3.java
+++ src/main/java/Day3.java
@@ -1,15 +1,29 @@
 public class Day3 {
     public static void main(String... args) {
         int input = 325489;
+        System.out.println(part1(input));
     }
 
     public static int part1(int input) {
         double sqrt = Math.sqrt(input - 1);
         int n = (int) Math.floor(sqrt);
         int k = sqrt % 1 < 0.5 ? n : n + 1;
-        int offset = input - 1 - n * k;
-        System.out.format("input: %d = %d * %d + 1 + %d\n", input, n, k, offset);
 
-        return 0;
+        int x = n % 2 == 0 ? n / (-2) : (n + 1) / 2;
+        int y = k % 2 == 0 ? k / 2 : k / (-2);
+
+        int offset = input - 1 - n * k;
+
+        System.out.format("input: %d = %d * %d + 1 + %d corner: [%2d:%-2d]", input, n, k, offset, x, y);
+
+        if (n != k) { // horizontal
+            x = y > 0 ? x - offset : x + offset;
+        } else {
+            y = x >= 0 ? y + offset : y - offset;
+        }
+
+        System.out.format(" coord: [%2d:%-2d]\n", x, y);
+
+        return Math.abs(x) + Math.abs(y);
     }
 }
--- src/test/java/Day3Test.java
+++ src/test/java/Day3Test.java
@@ -9,6 +9,14 @@ public class Day3Test {
     @Test
     public void part1() throws Exception {
         assertThat(Day3.part1(1), is(0));
+        assertThat(Day3.part1(2), is(1));
+        assertThat(Day3.part1(3), is(2));
+        assertThat(Day3.part1(4), is(1));
+        assertThat(Day3.part1(5), is(2));
+        assertThat(Day3.part1(6), is(1));
+        assertThat(Day3.part1(7), is(2));
+        assertThat(Day3.part1(8), is(1));
+        assertThat(Day3.part1(9), is(2));
         assertThat(Day3.part1(12), is(3));
         assertThat(Day3.part1(23), is(2));
         assertThat(Day3.part1(1024), is(31));
