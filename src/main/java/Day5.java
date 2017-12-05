6ef8ba1110cc619cbfef113258a567a5c6e14135
SpaceBison <combaine@gmail.com>
2017-12-05 18:41:51 +0100

    day 5 part 2

--- src/main/java/Day5.java
+++ src/main/java/Day5.java
@@ -2,11 +2,12 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/* there has to be a better way to do those :( */
 public class Day5 {
     public static void main(String... args) {
         String input = Common.getInputForDay(5);
         System.out.println(part1(input));
-        //System.out.println(part2(input));
+        System.out.println(part2(input));
     }
 
     public static int part1(String input) {
@@ -20,6 +21,23 @@ public class Day5 {
         while (0 <= index && index < jumps.size()) {
             int jump = jumps.get(index);
             jumps.set(index, jump + 1);
+            index += jump;
+            ++steps;
+        }
+        return steps;
+    }
+
+    public static int part2(String input) {
+        List<Integer> jumps = Arrays.stream(input.split("\n"))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
+
+        int index = 0;
+        int steps = 0;
+        while (0 <= index && index < jumps.size()) {
+            int jump = jumps.get(index);
+            jumps.set(index, jump >= 3 ? jump - 1 : jump + 1);
             index += jump;
             ++steps;
         }
--- src/test/java/Day5Test.java
+++ src/test/java/Day5Test.java
@@ -1,8 +1,7 @@
-import org.junit.Assert;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class Day5Test {
     @Test
@@ -11,4 +10,9 @@ public class Day5Test {
         assertThat(Day5.part1(input), is(5));
     }
 
+    @Test
+    public void part2() throws Exception {
+        String input = "0\n3\n0\n1\n-3";
+        assertThat(Day5.part2(input), is(10));
+    }
 }
