72a40a2aa717c4a004b20d6c73d156e50a16a4de
SpaceBison <combaine@gmail.com>
2017-12-05 17:53:32 +0100

    day 3 part 2

--- src/main/java/Day3.java
+++ src/main/java/Day3.java
@@ -1,7 +1,12 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Day3 {
     public static void main(String... args) {
         int input = 325489;
         System.out.println(part1(input));
+        System.out.println(part2(input));
     }
 
     public static int part1(int input) {
@@ -25,5 +30,73 @@ public class Day3 {
         System.out.format(" coord: [%2d:%-2d]\n", x, y);
 
         return Math.abs(x) + Math.abs(y);
+    }
+
+    /* This might need a cleanup */
+    public static int part2(int input) {
+        List<Integer> spiral = new LinkedList<>();
+        spiral.addAll(Arrays.asList(0, 1, 1, 2, 4, 5, 10, 11, 23));
+
+        for (int i = 0; i < spiral.size(); ++i) {
+            Integer value = spiral.get(i);
+            System.out.println("    init " + i + " " + value);
+            if (value > input) {
+                return value;
+            }
+        }
+
+        int a = 3;
+        int b = 3;
+        int corner = a * b;
+        int offset = 6;
+
+        int nextValue = 0;
+
+        int i = spiral.size();
+        do {
+            if (i == corner) {
+                ++offset;
+                nextValue = spiral.get(i - 1) + spiral.get(i - offset) + spiral.get(i - offset - 1);
+
+                if (nextValue > input) {
+                    return nextValue;
+                }
+
+                System.out.println("pre corner " + i + " " + nextValue);
+                spiral.add(nextValue);
+                ++i;
+
+                ++offset;
+
+                nextValue = nextValue + spiral.get(i - offset);
+                System.out.println("    corner " + i + " " + nextValue);
+                spiral.add(nextValue);
+                ++i;
+
+                if (nextValue > input) {
+                    return nextValue;
+                }
+
+                nextValue = 2 * nextValue + spiral.get(i - offset);
+                System.out.println("pos corner " + i + " " + nextValue);
+
+                if (a == b) {
+                    a++;
+                } else {
+                    b = a;
+                }
+                corner = a * b;
+            } else {
+                nextValue = nextValue + spiral.get(i - offset) + spiral.get(i - offset - 1) + spiral.get(i - offset - 2);
+                System.out.println("    normal " + i + " " + nextValue);
+            }
+
+            spiral.add(nextValue);
+            ++i;
+
+            //System.out.println(spiral.subList(spiral.size() - 5, spiral.size()));
+        } while (nextValue <= input);
+
+        return nextValue;
     }
 }
--- src/test/java/Day3Test.java
+++ src/test/java/Day3Test.java
@@ -1,9 +1,7 @@
-import org.hamcrest.CoreMatchers;
-import org.junit.Assert;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class Day3Test {
     @Test
@@ -21,4 +19,28 @@ public class Day3Test {
         assertThat(Day3.part1(23), is(2));
         assertThat(Day3.part1(1024), is(31));
     }
+
+    @Test
+    public void part2() throws Exception {
+        assertThat(Day3.part2(1), is(2));
+        assertThat(Day3.part2(2), is(4));
+        assertThat(Day3.part2(3), is(4));
+        assertThat(Day3.part2(4), is(5));
+        assertThat(Day3.part2(5), is(10));
+        assertThat(Day3.part2(7), is(10));
+        assertThat(Day3.part2(8), is(10));
+        assertThat(Day3.part2(9), is(10));
+        assertThat(Day3.part2(10), is(11));
+        assertThat(Day3.part2(11), is(23));
+        assertThat(Day3.part2(12), is(23));
+        assertThat(Day3.part2(13), is(23));
+        assertThat(Day3.part2(23), is(25));
+        assertThat(Day3.part2(800), is(806));
+    }
+
+    /*      147  142  133  122   59
+            304    5    4    2   57
+            330   10    1    1   54
+            351   11   23   25   26
+            362  747  806--->   ...*/
 }
