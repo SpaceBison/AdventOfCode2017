b0c68c0fb1d14dbcab5adb04a33fec9f59ffa7ed
SpaceBison <combaine@gmail.com>
2017-12-05 18:17:51 +0100

    day 4 part 2

--- src/main/java/Day4.java
+++ src/main/java/Day4.java
@@ -1,16 +1,31 @@
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.stream.Collectors;
 
 public class Day4 {
     public static void main(String... args) {
         String input = Common.getInputForDay(4);
         System.out.println(part1(input));
+        System.out.println(part2(input));
     }
 
     public static long part1(String input) {
         return Arrays.stream(input.split("\n"))
                 .map(line -> line.split(" "))
                 .filter(words -> new HashSet<>(Arrays.asList(words)).size() == words.length)
+                .count();
+    }
+
+    public static long part2(String input) {
+        return Arrays.stream(input.split("\n"))
+                .map(line -> line.split(" "))
+                .filter(words -> Arrays.stream(words)
+                        .map(word -> word.chars()
+                                .sorted()
+                                .boxed()
+                                .collect(Collectors.toList()))
+                        .distinct()
+                        .count() == words.length)
                 .count();
     }
 }
--- src/test/java/Day4Test.java
+++ src/test/java/Day4Test.java
@@ -5,6 +5,12 @@ import static org.junit.Assert.assertThat;
 
 public class Day4Test {
     @Test
+    public void part2() throws Exception {
+        String input = "abcde fghij\nabcde xyz ecdab\na ab abc abd abf abj\niiii oiii ooii oooi oooo\noiii ioii iioi iiio";
+        assertThat(Day4.part2(input), is(3L));
+    }
+
+    @Test
     public void part1() throws Exception {
         String input = "aa bb cc dd ee\naa bb cc dd aa\naa bb cc dd aaa";
         assertThat(Day4.part1(input), is(2L));
