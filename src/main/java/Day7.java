e6249de1376ce7f6efd92ba5c21826c011309d16
Wojciech Matuszewski <wmatuszewski@n7mobile.com>
2017-12-07 08:14:56 +0100

    day 7 part 2

--- src/main/java/Day7.java
+++ src/main/java/Day7.java
@@ -1,7 +1,6 @@
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Objects;
+
+import java.util.*;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Day7 {
@@ -29,7 +28,47 @@ public class Day7 {
         return nonLeaves.iterator().next();
     }
 
-    static String part2(String input) {
-        return null;
+    static int part2(String input) {
+        Set<String[]> parsed = Arrays.stream(input.split("\n"))
+                .map(l -> l.split("( \\()|(\\) -> )|\\)"))
+                .collect(Collectors.toSet());
+
+        Map<String, Integer> weights = parsed.stream().collect(Collectors.toMap(
+                a -> a[0],
+                a -> Integer.parseInt(a[1])));
+
+        HashMap<String, Integer> originalWeights = new HashMap<>(weights);
+
+        Map<String, Set<String>> children = parsed.stream().collect(Collectors.toMap(
+                a -> a[0],
+                a -> a.length == 2 ? Collections.emptySet() : Arrays.stream(a[2].split(", ")).collect(Collectors.toSet())));
+
+        while (true) {
+            for (Map.Entry<String, Set<String>> entry : children.entrySet()) {
+                String key = entry.getKey();
+                Set<String> value = entry.getValue();
+
+                if (!value.isEmpty() && value.stream().map(children::get).allMatch(Set::isEmpty)) { // only leaves
+
+                    if (value.stream().map(weights::get).distinct().count() != 1) { // one of them does not match
+                        Map<Integer, Set<String>> disksByWeight = value.stream().collect(Collectors.groupingBy(weights::get, Collectors.toSet()));
+                        Map.Entry<Integer, Set<String>> badDisk = disksByWeight.entrySet().stream().filter(e -> e.getValue().size() == 1).findFirst().get();
+
+                        String badDiskName = badDisk.getValue().iterator().next();
+                        Integer badWeight = badDisk.getKey();
+
+                        disksByWeight.remove(badWeight);
+                        Integer goodWeight = disksByWeight.keySet().iterator().next();
+
+                        Integer originalBadWeight = originalWeights.get(badDiskName);
+
+                        return originalBadWeight - (badWeight - goodWeight);
+                    } else {
+                        weights.put(key, weights.get(key) + value.stream().mapToInt(weights::get).sum());
+                        children.put(key, Collections.emptySet());
+                    }
+                }
+            }
+        }
     }
 }
--- src/test/java/Day7Test.java
+++ src/test/java/Day7Test.java
@@ -3,25 +3,28 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class Day7Test {
+
+    public static final String INPUT = "pbga (66)\n" +
+            "xhth (57)\n" +
+            "ebii (61)\n" +
+            "havc (66)\n" +
+            "ktlj (57)\n" +
+            "fwft (72) -> ktlj, cntj, xhth\n" +
+            "qoyq (66)\n" +
+            "padx (45) -> pbga, havc, qoyq\n" +
+            "tknk (41) -> ugml, padx, fwft\n" +
+            "jptl (61)\n" +
+            "ugml (68) -> gyxo, ebii, jptl\n" +
+            "gyxo (61)\n" +
+            "cntj (57)";
+
     @Test
     public void part1() throws Exception {
-        Assert.assertThat(Day7.part1("pbga (66)\n" +
-                "xhth (57)\n" +
-                "ebii (61)\n" +
-                "havc (66)\n" +
-                "ktlj (57)\n" +
-                "fwft (72) -> ktlj, cntj, xhth\n" +
-                "qoyq (66)\n" +
-                "padx (45) -> pbga, havc, qoyq\n" +
-                "tknk (41) -> ugml, padx, fwft\n" +
-                "jptl (61)\n" +
-                "ugml (68) -> gyxo, ebii, jptl\n" +
-                "gyxo (61)\n" +
-                "cntj (57)"), CoreMatchers.is("tknk"));
+        Assert.assertThat(Day7.part1(INPUT), CoreMatchers.is("tknk"));
     }
 
     @Test
     public void part2() throws Exception {
-
+        Assert.assertThat(Day7.part2(INPUT), CoreMatchers.is(60));
     }
 }
