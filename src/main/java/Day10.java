752d32e81e120009d9680989d79e2f416b6147f8
SpaceBison <combaine@gmail.com>
2017-12-10 15:03:29 +0100

    day 10 part 1

--- src/main/java/Day10.java
+++ src/main/java/Day10.java
@@ -1,25 +1,42 @@
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Day10 {
     public static void main(String... args) {
-        String input = Common.getInputForDay(0);
+        String input = Common.getInputForDay(10);
         System.out.println(part1(input));
         System.out.println(part2(input));
     }
 
     static int part1(String input) {
-
-        return 0;
+        return part1Impl(
+                IntStream.range(0, 256).boxed().collect(Collectors.toList()),
+                Arrays.stream(input.split(",")).map(Integer::new).collect(Collectors.toList()));
     }
 
     static int part1Impl(List<Integer> list, List<Integer> lengths) {
         int current = 0;
         int skip = 0;
         for (Integer length : lengths) {
-
+            reverseRange(list, current, current + length);
+            current += length + skip++;
         }
 
-        return 0;
+        return list.get(0) * list.get(1);
+    }
+
+    static <E> void reverseRange(List<E> list, int start, int end) {
+        int swaps = (end - start) / 2;
+        int size = list.size();
+        for (int i = 0; i < swaps; ++i) {
+            int first = (start + i) % size;
+            int second = (end - i - 1) % size;
+            E tmp = list.get(first);
+            list.set(first, list.get(second));
+            list.set(second, tmp);
+        }
     }
 
     static int part2(String input) {
--- src/test/java/Day10Test.java
+++ src/test/java/Day10Test.java
@@ -1,8 +1,28 @@
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class Day10Test {
     @Test
+    public void reverseRange() throws Exception {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+
+        Day10.reverseRange(list, 0, 3);
+        assertThat(list, is(Arrays.asList(3, 2, 1, 4, 5)));
+
+        Day10.reverseRange(list, 3, 7);
+        assertThat(list, is(Arrays.asList(5, 4, 1, 2, 3)));
+    }
+
+    @Test
     public void part1() throws Exception {
+        List<Integer> list = Arrays.asList(0, 1, 2, 3, 4);
+        int value = Day10.part1Impl(list, Arrays.asList(3, 4, 1, 5));
+        assertThat(value, is(12));
     }
 
     @Test
