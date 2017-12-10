6d4669dd5703b2344dc2b4d983157cc19e1a46fc
SpaceBison <combaine@gmail.com>
2017-12-10 18:04:56 +0100

    day 10 part 2

--- src/main/java/Day10.java
+++ src/main/java/Day10.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.OptionalInt;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -11,21 +12,42 @@ public class Day10 {
     }
 
     static int part1(String input) {
-        return part1Impl(
-                IntStream.range(0, 256).boxed().collect(Collectors.toList()),
-                Arrays.stream(input.split(",")).map(Integer::new).collect(Collectors.toList()));
-    }
+        List<Integer> list = IntStream.range(0, 256).boxed().collect(Collectors.toList());
+        List<Integer> parsedInput = Arrays.stream(input.split(",")).map(Integer::new).collect(Collectors.toList());
 
-    static int part1Impl(List<Integer> list, List<Integer> lengths) {
         int current = 0;
         int skip = 0;
-        for (Integer length : lengths) {
+        for (Integer length : parsedInput) {
             reverseRange(list, current, current + length);
             current += length + skip++;
         }
 
         return list.get(0) * list.get(1);
     }
+
+    static String part2(String input) {
+        List<Integer> list = IntStream.range(0, 256).boxed().collect(Collectors.toList());
+        List<Integer> parsedInput = input.chars().boxed().collect(Collectors.toList());
+        parsedInput.addAll(Arrays.asList(17, 31, 73, 47, 23));
+
+        int current = 0;
+        int skip = 0;
+        for (int i = 0; i < 64; ++i) {
+            for (Integer length : parsedInput) {
+                reverseRange(list, current, current + length);
+                current += length + skip++;
+            }
+        }
+
+        return IntStream.range(0, 16)
+                .mapToObj(chunk -> IntStream.range(0, 16)
+                        .map(i -> list.get(chunk * 16 + i))
+                        .reduce((a, b) -> a ^ b))
+                .map(OptionalInt::getAsInt)
+                .map(i -> String.format("%02x", i))
+                .collect(Collectors.joining());
+    }
+
 
     static <E> void reverseRange(List<E> list, int start, int end) {
         int swaps = (end - start) / 2;
@@ -37,9 +59,5 @@ public class Day10 {
             list.set(first, list.get(second));
             list.set(second, tmp);
         }
-    }
-
-    static int part2(String input) {
-        return 0;
     }
 }
--- src/test/java/Day10Test.java
+++ src/test/java/Day10Test.java
@@ -21,12 +21,21 @@ public class Day10Test {
     @Test
     public void part1() throws Exception {
         List<Integer> list = Arrays.asList(0, 1, 2, 3, 4);
-        int value = Day10.part1Impl(list, Arrays.asList(3, 4, 1, 5));
-        assertThat(value, is(12));
+        int current = 0;
+        int skip = 0;
+        for (Integer length : Arrays.asList(3, 4, 1, 5)) {
+            Day10.reverseRange(list, current, current + length);
+            current += length + skip++;
+        }
+        assertThat(list, is(Arrays.asList(3, 4, 2, 1, 0)));
     }
 
     @Test
     public void part2() throws Exception {
+        assertThat(Day10.part2("1,2,3"), is("3efbe78a8d82f29979031a4aa0b16a9d"));
+        assertThat(Day10.part2(""), is("a2582a3a0e66e6e86e3812dcb672a272"));
+        assertThat(Day10.part2("AoC 2017"), is("33efeb34ea91902bb2f59c9920caa6cd"));
+        assertThat(Day10.part2("1,2,4"), is("63960835bcdc130f0b66d7ff4f6a5a8e"));
     }
 
 }
